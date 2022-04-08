6da22e05035e11e16e6db500da045c2b993c23fe
Monisha <moninagaraj23@gmail.com>
2022-04-08 09:06:52 +0000

    Initial Commit

--- springapp/src/main/java/com/examly/springapp/exception/UserNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.examly.springapp.exception;
-
-
-    public class UserNotFoundException extends RuntimeException{
-        public UserNotFoundException(String message) {
-            super(message);
-        }
-    }
-
